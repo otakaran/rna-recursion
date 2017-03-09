f4150f63219b0b83bade1ccb363f5425495b7aa7
Otakar Andrysek <otakar@me.com>
2017-03-09 11:36:20 -0800

    Added documentation to code

--- AlignSequence.java
+++ AlignSequence.java
@@ -1,21 +1,27 @@
-// Import math for :)
+// Import statements
 import java.lang.Math;
 import java.util.Arrays;
 
+/**
+* Constructor for objects of class RNA
+*/
 public class AlignSequence
 {
-    // instance variables - replace the example below with your own
-    static String seq1 = "ABC";
-    static String seq2 = "BCA";
+    // Test Strings
+    static String seq1 = "ABC";     // Top string
+    static String seq2 = "BCA";     // Left string
 
     /**
      * Constructor for objects of class RNA
      */
     public AlignSequence()
     {
-        // initialise instance variables
+        // Initialise instance variables
     }
 
+    /**
+     * This method prints the matrix to the console
+     */
     public static void printMatrix(int[][] board)
     {
         System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
@@ -30,31 +36,40 @@ public class AlignSequence
         System.out.println("                                 +--------+\n\n\n\n");
     }
     
+    /**
+     * This method generates the solution matrix using the two strings
+     */
     public static int generateMatrix()
     {
+        // Find the length of both strings
         int len1 = seq1.length();
         int len2 = seq2.length();
         
+        // Create the table based on strings
         int[][] table = new int[len1 + 1][len2 + 1];
         int value;
         
         // Generate the outline of the matrix
         table[0][0]= 0;
+        
+        // Set first row in decrementing order
         for (int i = 1; i <= len1; i++) 
         {
             table[0][i] = table[0][i-1] + -1;
         }
+        
+        // Set first column in decrementing order
         for (int j = 1; j <= len2; j++) 
         {
             table[j][0] = table[j-1][0] + -1;
         }
         
-        printMatrix(table);
-        
+        // Fill in remainder of matrix based on rules
         for (int i = 1; i <= len1; i++) 
         {
             for (int j = 1; j <= len2; j++) 
             {
+                // Check existing values and decrement or incrememnt based on if values match from the sequences
                 if (seq1.charAt(i-1) == seq2.charAt(j-1)) 
                 {
                     value = 1;
@@ -63,17 +78,19 @@ public class AlignSequence
                 {
                     value = -1;
                 }
-                
+                // Set the location of the matrix to the highest possible value
                 table[i][j] = java.lang.Math.max((table[i][j-1] - 1), java.lang.Math.max((table[i-1][j] - 1), (table[i-1][j-1] + value)));
             }
         }
         
+        // Debug print
         printMatrix(table);
-        
-        // Generate remainder based on rules
-        return 1;
+        return 0;
     }
 
+    /**
+     * Main function that calls other functions
+     */
     public static void main()
     {
         generateMatrix();
